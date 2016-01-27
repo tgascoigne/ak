66b821d26462164305f7a87c506d39c575ccbcb4
Tom Gascoigne <tom@gascoigne.me>
2016-01-27 22:24:55 +0000

    Fix mmap arg access

--- kernel/syscall/mem.c
+++ kernel/syscall/mem.c
@@ -6,13 +6,22 @@
 #include <kernel/panic.h>
 #include <kernel/syscall/syscall.h>
 
+struct mmap_args {
+	void *addr;
+	size_t length;
+	int *prot;
+	int *flags;
+	int *fd;
+	off_t *offset;
+};
+
 void *sys_brk(vaddr_t brk) {
 	return (void *)task_brk(CurrentTask, brk);
 }
 
-void *sys_mmap(void *addr, size_t length, int *prot, int *flags, int *fd, off_t *offset) {
-	if ((*flags & MAP_ANONYMOUS) != 0) {
-		return sys_brk((vaddr_t)sys_brk(0) + (vaddr_t)length);
+void *sys_mmap(struct mmap_args *args) {
+	if ((*args->flags & MAP_ANONYMOUS) != 0) {
+		return sys_brk((vaddr_t)sys_brk(0) + (vaddr_t)args->length);
 	}
 	PANIC("sys_mmap: not implemented\n");
 	return (void *)-1;
@@ -20,7 +29,7 @@ void *sys_mmap(void *addr, size_t length, int *prot, int *flags, int *fd, off_t 
 
 static bool mem_syscall_init(void) {
 	syscall_register(SYS_BRK, (syscall_fn_t)sys_brk, 1);
-	syscall_register(SYS_MMAP, (syscall_fn_t)sys_mmap, 6);
+	syscall_register(SYS_MMAP, (syscall_fn_t)sys_mmap, 1);
 	early_malloc_disable();
 	return true;
 }
