e77a701d02ebea1f465dfd9b565f802d539fa994
Tom Gascoigne <tom@gascoigne.me>
2016-02-09 23:42:11 +0000

    Init gdt before idt

--- arch/x86/boot/init.c
+++ arch/x86/boot/init.c
@@ -11,11 +11,11 @@
 #include <kernel/proc/task.h>
 
 void arch_init(multiboot_info_t *mb_info) {
+	gdt_init();
 	idt_init();
 	multiboot_mmap(mb_info);
 	/* the first 4m is used for various devices and the kernel */
 	frame_set_range(0, 0x400000);
 	pg_init();
-	gdt_init();
 	pit_init();
 }
