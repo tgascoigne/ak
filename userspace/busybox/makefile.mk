083a9323e4fb3bcc8c6ff5da0e291a11b0c93aba
Tom Gascoigne <tom@gascoigne.me>
2016-02-22 18:43:59 +0000

    out of tree building for busybox

--- userspace/busybox/makefile.mk
+++ userspace/busybox/makefile.mk
@@ -1,8 +1,11 @@
 $(INITRD_DIR)/busybox:
 	(cd userspace/busybox && \
+	mkdir -p out; \
+	make O=./out ak_defconfig; \
+	cd out; \
 	LDFLAGS="-Xlinker -melf_i386" \
 	CFLAGS="-g -DHAVE_KILLPG -DHAVE_STRTOD -march=i686 -m32 -nostdlib -I /usr/i386-linux-uclibc/usr/include/ -L $$HOME/ak/uClibc/lib/" LIBS="$$HOME/ak/uClibc/lib/crt1.o -lm -lc -lgcc" \
 	make)
-	cp userspace/busybox/busybox $@
+	cp userspace/busybox/out/busybox $@
 
 USER_BINARIES += $(INITRD_DIR)/busybox
